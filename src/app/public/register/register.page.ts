a33c5adf8b0b9317ddc2b2e747292a6afea98151
jmeolmsted <jmeolmsted@users.noreply.github.com>
2019-05-20 15:27:44 -0700

    Login with appearance

---
+++ src/app/public/register/register.page.ts
@@ -0,0 +1,16 @@
+import { AuthenticationService } from './../../services/authentication.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  constructor(private authService: AuthenticationService) { }
+
+  ngOnInit() {
+  }
+
+}
