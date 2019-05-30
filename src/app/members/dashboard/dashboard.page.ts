7077cd31b4d03108e276d2d4f0b0e32e997ea343
jmeolmsted <jmeolmsted@users.noreply.github.com>
2019-05-30 12:56:50 -0700

    Language button

--- src/app/members/dashboard/dashboard.page.ts
+++ src/app/members/dashboard/dashboard.page.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
-
+  public text: string = 'Mudar Idioma';
+  public title: string = 'Pollution Map';
+  public display: string = 'Welcome the project more to be added in the future.';
   constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
@@ -17,4 +19,15 @@ export class DashboardPage implements OnInit {
     this.authService.logout();
   }
 
+  changeText(): void{
+    if(this.text === 'Mudar Idioma'){
+      this.text = 'Change Language';
+      this.title = 'Mapa de Poluição';
+      this.display = 'Bem-vindo o projeto mais a ser adicionado no futuro.';
+    } else {
+      this.text = 'Mudar Idioma';
+      this.title = 'Pollution Map';
+      this.display = 'Welcome the project more to be added in the future.';
+    }
+  }
 }
--- src/app/public/login/login.page.ts
+++ src/app/public/login/login.page.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  public text: string = 'Mudar Idioma';
+  public title: string = 'Login';
+  public email: string = "your@email.com";
+  public password: string = "Password";
   constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
@@ -15,6 +18,20 @@ export class LoginPage implements OnInit {
 
   login(){
     this.authService.login();
+  }
+
+  changeText(): void{
+    if(this.text === 'Mudar Idioma'){
+      this.text = 'Change Language';
+      this.title = 'Entrar';
+      this.email = "seu@email.com";
+      this.password = "Senha";
+    } else {
+      this.text = 'Mudar Idioma';
+      this.title = 'Login';
+      this.email = "your@email.com";
+      this.password = "Password";
+    }
   }
 
 }
